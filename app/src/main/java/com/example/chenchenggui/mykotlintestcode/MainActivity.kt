45cdd05c2475670f368899eea4c071dbd58ea3ee
chenchenggui <chenchenggui@qie.tv>
2018-10-18 14:47:31 +0800

    debug recyclerview

--- app/src/main/java/com/example/chenchenggui/mykotlintestcode/MainActivity.kt
+++ app/src/main/java/com/example/chenchenggui/mykotlintestcode/MainActivity.kt
@@ -1,12 +1,67 @@
 package com.example.chenchenggui.mykotlintestcode
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.BaseViewHolder
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val dataList =ArrayList<String>()
+        for (c in 'A'..'D'){
+            dataList.add(c.toString())
+        }
+        for ((index,str) in dataList.withIndex()){
+            Log.e("main","index=$index---value=$str")
+        }
+        val rvTest = rv_test
+        rvTest.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        rvTest.adapter = TestAdapter(R.layout.item, dataList)
+//        rvTest.adapter=RvAdapter(this,dataList)
+    }
+
+    class RvAdapter(var context:Context,var dataList: List<String>): RecyclerView.Adapter<RvAdapter
+    .RvViewHolder>() {
+        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RvViewHolder {
+            var view:View = LayoutInflater.from(context).inflate(R.layout.item, p0, false)
+            return RvViewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return dataList.size
+        }
+
+        override fun onBindViewHolder(p0: RvViewHolder, p1: Int) {
+            p0.name.text=dataList[p1]
+        }
+
+
+        class RvViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            var name: TextView = itemView.findViewById(R.id.tv_text)
+            var image: ImageView = itemView.findViewById(R.id.iv_img)
+
+        }
+    }
+
+    class TestAdapter(layoutResId: Int, data: List<String>) : BaseQuickAdapter<String,
+            BaseViewHolder>(layoutResId, data) {
+        override fun convert(helper: BaseViewHolder?, item: String?) {
+            Log.e("main","item=$item")
+            helper?.setText(R.id.tv_text, "icon-$item")
+        }
     }
 }
