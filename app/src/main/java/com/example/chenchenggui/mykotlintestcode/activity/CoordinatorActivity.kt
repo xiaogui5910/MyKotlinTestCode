efb4139f752ec4e0767207dbdebf2e2f414fc84c
chenchenggui <chenchenggui@qie.tv>
2020-11-24 10:29:06 +0800

    update coordinatorlayout

--- app/src/main/java/com/example/chenchenggui/mykotlintestcode/activity/CoordinatorActivity.kt
+++ app/src/main/java/com/example/chenchenggui/mykotlintestcode/activity/CoordinatorActivity.kt
@@ -2,12 +2,19 @@ package com.example.chenchenggui.mykotlintestcode.activity
 
 import android.content.Context
 import android.graphics.Color
+import android.graphics.Typeface
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.util.TypedValue
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.example.chenchenggui.mykotlintestcode.R
-import com.example.chenchenggui.mykotlintestcode.fragment.NobeFragment
+import com.example.chenchenggui.mykotlintestcode.fragment.NobleFragment
+import kotlinx.android.synthetic.main.layout_noble_user.*
 import net.lucode.hackware.magicindicator.MagicIndicator
 import net.lucode.hackware.magicindicator.ViewPagerHelper
 import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator
@@ -26,14 +33,31 @@ class CoordinatorActivity : BaseActivity() {
 
     override fun initView() {
         super.initView()
+        initPerson()
         initViewPager()
+    }
+
+    private fun initPerson() {
+        val content1 = "2121"
+        val content2 = "骑士"
+        val showText = getString(R.string.show_upgrade_text, content1, content2)
+        val startIndex1 = showText.indexOf(content1)
+        val startIndex2 = showText.indexOf(content2)
+
+        val lightColor = ContextCompat.getColor(this, R.color.noble_yellow_caa869)
+        val textSpanned1 = SpannableString(showText)
+        textSpanned1.setSpan(ForegroundColorSpan(lightColor),
+                startIndex1, startIndex1 + content1.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        textSpanned1.setSpan(ForegroundColorSpan(lightColor),
+                startIndex2, startIndex2 + content2.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        tv_noble_upgrade.text = textSpanned1
     }
 
     private fun initViewPager() {
         val mViewPager = findViewById<View>(R.id.view_pager) as ViewPager
         val fragmentList = arrayListOf<Fragment>()
         mTitleDataList.forEach {
-            fragmentList.add(NobeFragment.newInstance(it))
+            fragmentList.add(NobleFragment.newInstance(it))
         }
         val adapter = object : FragmentPagerAdapter(supportFragmentManager) {
             override fun getCount(): Int {
@@ -64,7 +88,9 @@ class CoordinatorActivity : BaseActivity() {
             override fun getTitleView(context: Context?, index: Int): IPagerTitleView? {
                 val colorTransitionPagerTitleView = ColorTransitionPagerTitleView(context)
                 colorTransitionPagerTitleView.normalColor = Color.WHITE
-                colorTransitionPagerTitleView.selectedColor = Color.YELLOW
+                colorTransitionPagerTitleView.selectedColor = ContextCompat.getColor(context!!, R.color.noble_yellow_caa869)
+                colorTransitionPagerTitleView.typeface = Typeface.DEFAULT_BOLD
+                colorTransitionPagerTitleView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14f)
                 colorTransitionPagerTitleView.setText(mTitleDataList[index])
                 colorTransitionPagerTitleView.setOnClickListener { mViewPager.currentItem = index }
                 return colorTransitionPagerTitleView
--- app/src/main/java/com/example/chenchenggui/mykotlintestcode/fragment/NobleFragment.kt
+++ app/src/main/java/com/example/chenchenggui/mykotlintestcode/fragment/NobleFragment.kt
@@ -1,15 +1,14 @@
 package com.example.chenchenggui.mykotlintestcode.fragment
 
-import android.graphics.Color
 import android.os.Bundle
 import android.text.SpannableString
 import android.text.Spanned
-import android.text.style.BackgroundColorSpan
 import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -18,10 +17,10 @@ import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.example.chenchenggui.mykotlintestcode.R
 
 
-class NobeFragment : Fragment() {
+class NobleFragment : Fragment() {
     companion object {
-        fun newInstance(type: String): NobeFragment {
-            val dialogFragment = NobeFragment()
+        fun newInstance(type: String): NobleFragment {
+            val dialogFragment = NobleFragment()
             val bundle = Bundle()
             bundle.putString("type", type)
             dialogFragment.arguments = bundle
@@ -32,48 +31,57 @@ class NobeFragment : Fragment() {
     lateinit var recyclerview: RecyclerView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val rootView = inflater.inflate(R.layout.fragment_nobe, null)
+        val rootView = inflater.inflate(R.layout.fragment_noble, null)
         recyclerview = rootView.findViewById<RecyclerView>(R.id.recyclerview)
-//        val tvTitle=rootView.findViewById<TextView>(R.id.tv_title)
         arguments?.let {
             val string = it.getString("type")
-//            tvTitle.text = string
         }
         initView(rootView)
         return rootView
     }
 
     private fun initView(rootView: View) {
-        val nobeAdapter = NobeAdapter()
+        val nobleAdapter = NobleAdapter()
 
         //头布局
-        val headerView = LayoutInflater.from(context).inflate(R.layout.header_nobe, null)
-        val tvHeader = headerView.findViewById<TextView>(R.id.tv_header)
+        val headerView = LayoutInflater.from(context).inflate(R.layout.header_noble, null)
+        val tvHeader = headerView.findViewById<TextView>(R.id.tv_noble_header)
+        val tvHeaderDesc = headerView.findViewById<TextView>(R.id.tv_noble_header_desc)
         val content1 = "1000"
         val content2 = "大礼包"
         val showText = getString(R.string.show_text, content1, content2)
         val startIndex1 = showText.indexOf(content1)
         val startIndex2 = showText.indexOf(content2)
 
+        val lightColor = ContextCompat.getColor(context!!, R.color.noble_yellow_caa869)
         val textSpanned1 = SpannableString(showText)
-        textSpanned1.setSpan(ForegroundColorSpan(Color.RED),
+        textSpanned1.setSpan(ForegroundColorSpan(lightColor),
                 startIndex1, startIndex1 + content1.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-        textSpanned1.setSpan(ForegroundColorSpan(Color.YELLOW),
+        textSpanned1.setSpan(ForegroundColorSpan(lightColor),
                 startIndex2, startIndex2 + content2.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         tvHeader.text = textSpanned1
-        nobeAdapter.addHeaderView(headerView)
+
+        val content3 = "100/200"
+        val showText2 = getString(R.string.show_privilege_text, content3)
+        val startIndex3 = showText2.indexOf(content3)
+        val textSpanned3 = SpannableString(showText2)
+        textSpanned3.setSpan(ForegroundColorSpan(lightColor),
+                startIndex3, startIndex3 + content3.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        tvHeaderDesc.text = textSpanned3
+
+        nobleAdapter.addHeaderView(headerView)
 
         recyclerview.layoutManager = GridLayoutManager(context, 3, GridLayoutManager.VERTICAL, false)
-        recyclerview.adapter = nobeAdapter
+        recyclerview.adapter = nobleAdapter
 
         val dataList = arrayListOf<String>()
-        for (i in 0..50) {
+        for (i in 0..12) {
             dataList.add(i.toString())
         }
-        nobeAdapter.setNewInstance(dataList)
+        nobleAdapter.setNewInstance(dataList)
     }
 
-    class NobeAdapter : BaseQuickAdapter<String, BaseViewHolder>(R.layout.item_nobe) {
+    class NobleAdapter : BaseQuickAdapter<String, BaseViewHolder>(R.layout.item_noble) {
         override fun convert(holder: BaseViewHolder, item: String) {
             holder.setText(R.id.tv_item, "item$item")
         }
